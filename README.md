523db5e891a4e4a758319ddeeb42a1b09dae8573
Katherine Rinas <katierinas@me.com>
2018-03-16 14:46:33 -0600

    keep forgetting to push changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NewsScraper
+
+Search, scrape, and save articles from The New York Times! 
---
+++ server.js
@@ -0,0 +1,109 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require ("axios");
+var PORT = 3000;
+
+
+var app = express();
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost:27017/newsscraper");
+//this route is probably wrong! Also, need to write code to push result object into results array.
+
+var db = require("./models");
+
+
+app.get("/scrape", function(req, res) {
+  
+  axios.get("http://www.nytimes.com/").then(function(response) {
+    
+    var $ = cheerio.load(response.data);
+
+ var resultsArr = [];
+    
+    $(".theme-summary").each(function(i, element) {
+     
+      var result = {};
+
+      result.title = $(this)
+        .children(".story-heading")
+        .text();
+      result.link = $(this)
+        .children(".story-heading")
+        .children("a")
+        .attr("href");
+
+        console.log(result);
+
+     
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+         console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.error(err);
+          return res.json(err);
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  console.log("Here");
+  
+  db.Article.find({})
+    .then(function(dbArticle) {
+      
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+     
+      res.json(err);
+    });
+});
+
+
+app.get("/articles/:id", function(req, res) {
+  
+  db.Article.findOne({ _id: req.params.id })
+    
+    .populate("note")
+    .then(function(dbArticle) {
+      
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      
+      res.json(err);
+    });
+});
+
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+     
+      return db.Article.findOneAndUpdate({ _id: req.params.id}, {note: dbNote._id}, {new: true });
+    })
+    .then(function(dbArticle) {
+       res.json(dbArticle);
+    })
+    .catch(function(err) {
+       res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
